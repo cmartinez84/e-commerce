3438b1b08620f97e7fb4063b09660505b750a153
chris martinez and justin kincaid <justin@>
2016-11-02 10:20:02 -0700

    add application route

---
+++ app/routes/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      items: this.store.findAll('item'),
+      categories: this.store.findAll('category')
+    });
+  }
+});
